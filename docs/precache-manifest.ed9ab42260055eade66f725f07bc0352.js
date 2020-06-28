b8d23be9f1fc0389ed1b36dddc0133f658d0aedd
kelgar293 <kelgar293@icloud.com>
2020-06-28 14:26:08 -0600

    Docs agregado

---
+++ docs/precache-manifest.ed9ab42260055eade66f725f07bc0352.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8618ade2831aab24a7b9925b76d7c514",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d578dc6613f64aadeee9",
+    "url": "/static/css/main.de797570.chunk.css"
+  },
+  {
+    "revision": "85a4a86f3fe06f17fcc7",
+    "url": "/static/js/2.8249eaeb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8249eaeb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d578dc6613f64aadeee9",
+    "url": "/static/js/main.c55f720c.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
